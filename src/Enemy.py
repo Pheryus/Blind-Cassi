e5e288a9da60c334bbe8e48b66a61625c4b6c933
Pheryus <plima@inf.ufsm.br>
2017-01-21 16:15:29 +0300

    Go Enemy

--- src/Enemy.py
+++ src/Enemy.py
@@ -15,16 +15,17 @@ class Enemy(GameObject):
     def __init__(self, game_data, pos, state):
         GameObject.__init__(self, None, game_data)
 
-        self.animation_names = ['stand_up', 'stand_down', 'stand_left', 'stand_right',
-                            'walking_left', 'walking_right', 'walking_down', 'walking_up']
+        #self.animation_names = ['stand_up', 'stand_down', 'stand_left', 'stand_right',
+         #                   'walking_left', 'walking_right', 'walking_down', 'walking_up']
         self._layer = 2
 
 
-        self.dest = pygame.rect(pos[0], pos[1], 0, 0)
-        self.scale = 0.5
+        self.dest = pygame.Rect(pos[0], pos[1], 0, 0)
+        #self.scale = 0.5
         self.state = state
 
-
+    def render(self):
+        pass
 
     def update(self):
         pass
